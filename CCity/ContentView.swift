0dde69c172e8bfd7e00f9ebeeefe0bdb289e28a9
ataka <masayuki.ataka@gmail.com>
2019-07-21 15:09:40 +0900

    Initial Commit

---
+++ CCity/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CCity
+//
+//  Created by 安宅 正之 on 2019/07/21.
+//  Copyright © 2019 WelltempRed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
